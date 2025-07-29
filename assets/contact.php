c184adc42ee6a6979e44fa366b22699ceaf9d20b
LoganHoney <honey.logan.t@gmail.com>
2025-07-29 12:04:54 -0400

    Update contact.php

--- assets/contact.php
+++ assets/contact.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "email@gmail.com";
+        $recipient = "hernandoinspections@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
